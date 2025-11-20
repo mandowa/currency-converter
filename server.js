459437b0e72353236f4b4264a353ce83176c31dc
Kim Liu <kimlcs@SMT-KimL-A2.local>
2025-11-20 12:01:52 +0800

    Bind server to 0.0.0.0 for AWS App Runner

--- server.js
+++ server.js
@@ -108,6 +108,6 @@ const server = http.createServer(async (req, res) => {
     });
 });
 
-server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
